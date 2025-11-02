77644c77c0ecef48f8412bc80355e56268e46733
FrozenFrost6 <ch.anvesh2002@gmail.com>
2025-11-02 11:26:38 -0600

    Added 2 projects and added more more dances, added technologies in description for each project

--- src/components/Dance.jsx
+++ src/components/Dance.jsx
@@ -6,6 +6,11 @@ import varnam from "../assets/varnam2_thumbnail_1.png";
 import brahmam from "../assets/brahmamokate_thumbnail.jpg";
 import pushpanjali from "../assets/Pushpanjali_2.jpg";
 import shabdham from "../assets/Shabdham_1.png";
+import pushpanjaliAadhi from "../assets/pushpanjaliRaagam.jpg"
+import kumaraSambhavam from "../assets/kumaraSambhavamRaagam.jpg"
+import thillanaHindholam from "../assets/thillanaRaagam.jpg"
+import shriChakraRaaja from "../assets/shriChakraRaaja.jpg"
+import kaavadiChindu from "../assets/kaavadiChindu.jpg"
 
 const Dance = () => {
   const projects = [
@@ -32,6 +37,36 @@ const Dance = () => {
       title: "Venuganane Shabdham",
       desc: "INSERT DESCRIPTION HERE",
       youtube: "https://www.youtube.com/watch?v=3xHD7CxjsLQ",
+    },
+    {
+      image: pushpanjaliAadhi,
+      title: "Pushpanjali Gambheera Naattai Aadhi",
+      desc: "INSERT DESCRIPTION HERE",
+      youtube: "",
+    },
+    {
+      image: kumaraSambhavam,
+      title: "Kumara Sambhavam",
+      desc: "INSERT DESCRIPTION HERE",
+      youtube: "",
+    },
+    {
+      image: thillanaHindholam,
+      title: "Thillana Hindholam",
+      desc: "INSERT DESCRIPTION HERE",
+      youtube: "",
+    },
+    {
+      image: shriChakraRaaja,
+      title: "Shri Chakra Raaja",
+      desc: "INSERT DESCRIPTION HERE",
+      youtube: "",
+    },
+    {
+      image: kaavadiChindu,
+      title: "Kaavadi Chindu",
+      desc: "INSERT DESCRIPTION HERE",
+      youtube: "",
     }
   ];
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import carbonFootprintPic from "../assets/carbonFootprint.png";
 import nmapPic from "../assets/nmap.png";
 import digitalPaperNotesPic from "../assets/digitalPaperNotes.jpg";
 import wordDevilPic from "../assets/wordDevil.png";
+import personalPortfolioPic from "../assets/personalPortfolioPic.jpg"
 import { CiTextAlignLeft } from "react-icons/ci";
 
 const Projects = () => {
@@ -13,33 +14,45 @@ const Projects = () => {
     {
       title:
         "Cutesy Digital Paper-pad with Animated Writing and Scratch Sounds",
-      desc: "As users type, text is rendered as animated handwriting strokes on digital lined paper, while synchronized pencil scratch audio creates the illusion of physically writing on a notebook.",
+      desc: "As users type, text is rendered as animated handwriting strokes on digital lined paper, while synchronized pencil scratch audio creates the illusion of physically writing on a notebook. Tech used: React, Tailwind CSS, Typescript, Howler.js",
       img: digitalPaperNotesPic,
       github: "https://github.com/dejah22/digital_paper_notes",
     },
     {
       title: "NetMapper",
-      desc: "Practical Experiments in Network Protocols and Attacks (Similar to Nmap)",
+      desc: "Practical Experiments in Network Protocols and Attacks (Similar to Nmap). Tech used: C/C++, POSIX Networking APIs",
       img: nmapPic,
       github: "https://github.com/dejah22/NetMapper",
     },
     {
       title: "Pretty Notes app",
-      desc: "An intuitive, minimalist and visually appealing note-making app for seamless note-taking.",
+      desc: "An intuitive, minimalist and visually appealing note-making app for seamless note-taking. Tech used: React, CSS, Springboot, Axios",
       img: noteAppPic,
       github: "https://github.com/dejah22/Pretty_Notes-FE",
     },
     {
       title: "Carbon Footprint Tracker",
-      desc: "End-to-end browser app that calculates and tracks your carbon footprint, with the aim of fostering a greener planet",
+      desc: "End-to-end browser app that calculates and tracks your carbon footprint, with the aim of fostering a greener planet. Tech used: Apache Tomcat Servlet Container, Java, MySQL, HTML, CSS",
       img: carbonFootprintPic,
       github: "https://github.com/dejah22/Carbon-Footprint-Tracker",
     },
     {
       title: "Word devil",
-      desc: "A spooky twist to the popular game Hangman, built using primitive Turbo C++ during my school days!",
+      desc: "A spooky twist to the popular game Hangman, built using primitive Turbo C++ during my school days!. Tech used: Turbo C++",
       img: wordDevilPic,
       github: "https://github.com/dejah22/Word-Devil",
+    },
+    {
+      title: "Live Pose Estimation and Feedback for Bharatanatyam Dance Poses",
+      desc: "This project uses computer vision and AI-based similarity scoring to evaluate Bharatanatyam dance postures in real-time. By normalizing human body landmarks, the system provides pose-invariant comparisons and dynamic feedback to learners and practioners. Switch on your webcam and strike a pose!. Tech used: Python, MediaPipe, OpenCV, Numpy, TensorFlow.",
+      img: wordDevilPic,
+      github: "https://github.com/dejah22/live-pose-estimation-feedback-for-dance-poses",
+    },
+    {
+      title: "Personal Portfolio",
+      desc: "A modern, responsive personal portfolio website built to showcase my projects, skills, and experience. The portfolio serves as a central hub where visitors can learn more about me, explore my work, and get in touch. Tech used: React, Vite, CSS",
+      img: personalPortfolioPic,
+      github: "https://github.com/dejah22/dejah22.github.io",
     },
   ];
 
