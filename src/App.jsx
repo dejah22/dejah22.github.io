ccf0d03f5a4170121a47fcfca73bd64e72a98cd3
Dejah <137484467+dejah22@users.noreply.github.com>
2025-11-07 19:10:33 +0530

    build gone

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import Home from "./components/Home";
@@ -11,7 +11,7 @@ import "./App.css";
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <div className="app-layout">
         <Sidebar />
         <div className="main-content">
@@ -26,7 +26,7 @@ function App() {
           </Routes>
         </div>
       </div>
-    </Router>
+    </HashRouter>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { HashRouter } from "react-router-dom";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <StrictMode>
     <App />
-  </HashRouter>
+  </StrictMode>
 );
